2998a84c3215ea3e4d13a7a3219c831009e0a6b2
Harshh018 <hg053077@gmail.com>
2026-09-03 23:30:08 +0530

    Fix CORS and update static catch-all route

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-const connectDatabase=()=>{
-
-mongoose.connect(process.env.DB_URI, {}).then((data)=>{
-    console.log(`mongodb connected with server:${data.connection.host}`);
-})
-}
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      dbName: "Ecommerce", // Hardcodes target database to Ecommerce
+    })
+    .then((data) => {
+      console.log(`MongoDB connected with server: ${data.connection.host}`);
+      console.log(`Active Database Name: ${data.connection.name}`);
+    })
+    .catch((err) => {
+      console.error(`MongoDB Connection Error: ${err.message}`);
+    });
+};
 
 module.exports = connectDatabase;
-
