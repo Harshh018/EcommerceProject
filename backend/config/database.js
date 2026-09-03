75ba7b34554ff4d5a8df75bb8241f0bd35eac7e7
Harshh018 <hg053077@gmail.com>
2026-09-03 23:51:28 +0530

    Override DB_URI to aoowl3r cluster

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
+  // Hardcoded direct connection to your aoowl3r cluster
+  const DB_URI = "mongodb+srv://harshgupta:Harsh12345@cluster0.aoowl3r.mongodb.net/Ecommerce?appName=Cluster0";
+
   mongoose
-    .connect(process.env.DB_URI, {
-      dbName: "Ecommerce", // Hardcodes target database to Ecommerce
+    .connect(DB_URI, {
+      dbName: "Ecommerce",
     })
     .then((data) => {
       console.log(`MongoDB connected with server: ${data.connection.host}`);
-      console.log(`Active Database Name: ${data.connection.name}`);
     })
     .catch((err) => {
       console.error(`MongoDB Connection Error: ${err.message}`);
