affecc06c4193ff11df956f9e4b1faa4249f2537
Harshh018 <hg053077@gmail.com>
2026-09-06 21:51:32 +0530

    update ProductDetails.css

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -111,6 +111,21 @@ export const logoutUser = () => async (dispatch) => {
   }
 };
 
+// Logout User
+export const logout = () => async (dispatch) => {
+  try {
+    localStorage.removeItem("shippingInfo");
+    localStorage.removeItem("cartItems");
+    sessionStorage.removeItem("orderInfo");
+
+    await axios.get(`/api/v1/logout`);
+
+    dispatch({ type: LOGOUT_SUCCESS });
+  } catch (error) {
+    dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+  }
+};
+
 // UPDATE PROFILE
 export const updateProfile = (userData) => async (dispatch) => {
   try {
