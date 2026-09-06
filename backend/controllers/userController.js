c222293016dcc1c3449306ebc8db0bc922727d28
Harshh018 <hg053077@gmail.com>
2026-09-06 21:53:06 +0530

    update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -97,6 +97,21 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
 });
 
 
+// Logout User
+export const logout = () => async (dispatch) => {
+  try {
+    localStorage.removeItem("shippingInfo");
+    localStorage.removeItem("cartItems");
+    sessionStorage.removeItem("orderInfo");
+
+    await axios.get(`/api/v1/logout`);
+
+    dispatch({ type: LOGOUT_SUCCESS });
+  } catch (error) {
+    dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+  }
+};
+
 // ================= FORGOT PASSWORD =================
 
 exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
